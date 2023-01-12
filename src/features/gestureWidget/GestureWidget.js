ece3e0fe68c6ff3b072195f83fafdd63d7275786
mattvlaw <mattvlaw@gmail.com>
2023-01-11 21:41:05 -0500

    updates to gesture widget

--- src/features/gestureWidget/GestureWidget.js
+++ src/features/gestureWidget/GestureWidget.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import {Paper, Select, FormControl, Typography} from '@mui/material';
 import PlayControls from '../gui/PlayControls';
+import { addNewGesture } from './GestureSlice';
 
 const gestures = [
     "Nod",
@@ -11,7 +12,7 @@ const gestures = [
 ]
 export default function GestureWidget(props){
     return <Paper>
-            <Typography variant="h4">Gesture</Typography>
+            <Typography variant="h4">Gestures</Typography>
             <FormControl sx={{ width: "100%" }}>
                 <Select
                     multiple
@@ -21,9 +22,31 @@ export default function GestureWidget(props){
                         <option key={gesture} value={gesture}>
                             {gesture}
                         </option>
-                    ))}
+                    ))}reeText
                 </Select>
             <PlayControls />
+            <Stack direction="row" spacing={8}>
+                    <PlayControls handlePlay={handleSpeak} handleClear={handleClear}/>
+                    <Stack direction="row">
+                        <IconButton
+                            onClick={()=>dispatch(addNewGesture(f))}
+                        >
+                            <Save />
+                        </IconButton>
+                        <IconButton
+                            onClick={()=>dispatch(removeFromPhraseList(phraseIdx))}
+                        >
+                            <Delete />
+                        </IconButton>
+                        <IconButton
+                            onClick={()=>{setPhraseIdx(-1); dispatch(setPhraseList([]));}}
+                        >
+                            <Refresh />
+                        </IconButton>
+
+                    </Stack>
+                    
+                </Stack>
             </FormControl>
         </Paper>
 }
